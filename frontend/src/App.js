61c434af65b3fbb37ce6c703b15f4335d44fa32f
rayen <raynlazghab@gmail.com>
2022-04-30 22:09:34 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href='/'>amazona</a>
+      </header>
+      <main>list products</main>
+    </>
+  )
+}
+
+export default App
