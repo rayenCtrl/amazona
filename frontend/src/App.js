2d084fe42c56cabdb1eeb8cf5a0def0ac9b4aa1c
rayen <raynlazghab@gmail.com>
2022-05-01 11:42:25 +0100

    first -commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to='/'>amazona</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
@@ -15,7 +24,10 @@ function App() {
             <Route path='/' element={<HomeScreen />} />
           </Routes>
         </main>
-      </>
+        <footer>
+          <div className='text-center'>All rights reserved.</div>
+        </footer>
+      </div>
     </BrowserRouter>
   )
 }
