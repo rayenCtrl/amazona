808a4dccfaf31323ea1dc79cee2f5b04ae5080a9
rayen <raynlazghab@gmail.com>
2022-05-01 23:53:52 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,14 @@ app.get('/api/products/slug/:slug', (req, res) => {
   }
 })
 
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id)
+  if (product) {
+    res.send(product)
+  } else {
+    res.status(404).send({ message: 'Product not found' })
+  }
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server 🏃 on http://localhost:${port}`))
