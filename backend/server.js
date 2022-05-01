61bdd08f57ad4ce06b17dd5b81aca6890a67d33d
rayen <raynlazghab@gmail.com>
2022-05-01 01:11:23 +0100

    axios implementation

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import data from './data.js'
 
 const app = express()
 
-app.get('/api/products', (_req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.product)
 })
 
 const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Server 🏃 on port ${port}`))
+app.listen(port, () => console.log(`Server 🏃 on http://localhost:${port}`))
