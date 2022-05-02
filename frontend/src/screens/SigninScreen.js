5bce0b4d12354770d8b6663cb0380d0e4ac3683a
rayen <raynlazghab@gmail.com>
2022-05-02 21:32:23 +0100

    siginHunling

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,27 +1,68 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios'
 import { Container, Form, Button } from 'react-bootstrap'
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation, Link, useNavigate } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
+import { Store } from '../Store'
+import { toast } from 'react-toastify'
+import { getError } from '../utils'
 
 const SigninScreen = () => {
+  const navigate = useNavigate()
   // it's a hook from react router dom that will be called when the component is rendered
   const { search } = useLocation()
   const redirectInUrl = new URLSearchParams(search).get('redirect')
   const redirect = redirectInUrl ? redirectInUrl : '/'
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const { userInfo } = state
+
+  const submitHandler = async (e) => {
+    e.preventDefault() // prevent refreshing the page when submitting the form
+    try {
+      const { data } = await axios.post('/api/users/signin', {
+        email,
+        password,
+      })
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data })
+      localStorage.setItem('userInfo', JSON.stringify(data))
+      navigate(redirect || '/')
+    } catch (err) {
+      toast.error(getError(err))
+    }
+  }
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [navigate, redirect, userInfo])
+
   return (
     <Container className='small-container'>
       <Helmet>
         <title>Sign In</title>
       </Helmet>
       <h2 className='my-3'>Sign In</h2>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className='mb-3' controlId='email'>
           <Form.Label>Email</Form.Label>
-          <Form.Control type='email' required />
+          <Form.Control
+            type='email'
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='password'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type='password' required />
+          <Form.Control
+            type='password'
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <div className='mb-3'>
           <Button variant='primary' type='submit'>
