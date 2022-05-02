181964eef4c1509cc7546758f27cf31fb0fbfd53
rayen <raynlazghab@gmail.com>
2022-05-02 16:52:20 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import data from './data.js'
 import seedRouter from './routes/seedRoutes.js'
 import productRouter from './routes/productRoutes.js'
 
